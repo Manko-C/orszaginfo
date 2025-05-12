b3d2e1df8534fcaf42cc4bb584b8db109d3c6a80
Manko-C <csalamartin2@gmail.com>
2025-05-12 14:18:22 +0200

    megjelenites kesz

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import Orszagsigma from './components/Orszagsigma';
 function App() {
-
+  return(
+  <Orszagsigma />
+)
 }
 
 export default App;
--- src/components/Orszagsigma.jsx
+++ src/components/Orszagsigma.jsx
@@ -1,5 +1,22 @@
 import "../components/Orszagsigma.css"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 export default function Orszagsigma(){
-
+    const [orszag,setOrszag] = useState(null)
+    useEffect (() =>{
+        fetch("https://restcountries.com/v3.1/all")
+        .then((res) => res.json())
+        .then((data) => setOrszag(data))
+        .catch((err) => console.error("Fetch hiba:", err))
+    } ,[])
+    return(
+        <>
+        <h1>Ország Információ</h1>
+        <input type="text" placeholder="keresés" id="kereso"></input>
+        <div id="fodiv">
+            {orszag  && orszag.map((orszagok, index) =>(
+                <div className="orszag_div" key={index}>{orszagok.name.official} {orszagok.capital} {orszagok.region} <img src={orszagok.flags.png}></img></div>
+            ))}
+        </div>
+        </>
+    )
 }
