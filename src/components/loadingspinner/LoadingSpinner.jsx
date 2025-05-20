badd607d7b44b682c689e176ca091950d3d661af
kadam1222 <k.adam12220@gmail.com>
2025-05-20 11:13:08 +0200

    asd

---
+++ src/components/loadingspinner/LoadingSpinner.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Loadingspinner.css"
+
+
+const LoadingSpinner = () =>{
+    return (
+        <div className="spinner-container">
+            <div className="loading-spinner"></div>
+        </div>
+        
+    )
+}
+
+export default LoadingSpinner;
